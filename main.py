0167e4f0e4f9b17786137d33d0300dbaf3eaddb4
hypeoman <surgu4ev.m@yandex.ru>
2024-01-31 17:12:24 +0300

    get_youtube_video function written

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher, Router, types
+from aiogram import F
+from aiogram.types import FSInputFile
 from pytube import YouTube
+from aiogram.types import URLInputFile
 import sys
 import logging
 import asyncio
@@ -22,13 +25,18 @@ dp = Dispatcher()
 def get_tiktok_video() -> None:
     pass
 
-#! TODO: Write this function
-def get_youtube_video(url) -> None:
+#! function to get video from youtube
+def get_youtube_video(url) -> str:
     youtubeObject = YouTube(url)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    youtubeObject = youtubeObject.streams.filter(
+        file_extension="mp4").get_highest_resolution()
     try:
         youtubeObject.download()
         logging.info("Download is completed successfully")
+        path = f"{youtubeObject.title}.mp4"
+        logging.info(f"Filepath got correctly: {path}")
+
+        return path
     except:
         logging.error("An error has occured while downloading youtube video.")
 
@@ -47,7 +55,9 @@ async def url_handler(message: types.Message):
         url = message.text
 
         try:
-            get_youtube_video(url)
+            filepath = get_youtube_video(url)
+            video_to_send = FSInputFile(filepath)
+            await message.answer_video(video_to_send)
         except:
             try:
                 get_tiktok_video(url)
@@ -59,10 +69,22 @@ async def url_handler(message: types.Message):
     except:
         pass
 
-#! Main message_handler
-@dp.message()
-async def message_handler(message: types.Message):
-    await message.answer(f"Hello!")
+#! TODO
+def translate_to_selected_language(text) -> str:
+    pass
+
+#! /start command handler
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, I can download videos from tiktok, youtube, youtube shorts and reels!")
 
 #! Main function
 async def main() -> None:
