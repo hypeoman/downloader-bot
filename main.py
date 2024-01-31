c0a5deb84a530a80e97acf8d820018f1d0d764ff
exlag <kirill.pulin@gmail.com>
2024-01-31 13:53:46 +0300

    clone main code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from aiogram.types import URLInputFile, MessageEntity
 from aiogram.utils.markdown import hbold
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher, Router, types
-import re
-import requests
+from pytube import YouTube
 import sys
 import logging
 import asyncio
@@ -15,64 +13,65 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 API_TOKEN = config["API"]["TOKEN"]
+#test
 
-
-#! # All handlers should be attached to the Dispatcher
+#! All handlers should be attached to the Dispatcher
 dp = Dispatcher()
 
-print(API_TOKEN)
+#! TODO: Write this function
+def get_tiktok_video() -> None:
+    pass
 
+#! TODO: Write this function
+def get_youtube_video(url) -> None:
+    youtubeObject = YouTube(url)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+        logging.info("Download is completed successfully")
+    except:
+        logging.error("An error has occured while downloading youtube video.")
 
-@dp.message()
-async def message_handler(message: types.Message):
-    await message.answer(f"Привет! Отправь мне ссылку на TikTok")
+#! TODO: Write this function
+def get_reels_video() -> None:
+    pass
 
-
+#! Url handler
 @dp.message()
 async def url_handler(message: types.Message):
-    url = message
-    entities = message.entities
-    for entity in entities:
-        if entity.type == MessageEntity.URL:
-            print('ссылка')
-            get_tiktok_video_id(url)
-            tiktok(url)
+    #! This hadnler try get videos from:
+    #! 1. youtube (with shorts);
+    #! 2. tiktok;
+    #! 3. reels;
+    try:
+        url = message.text
 
-def get_tiktok_video_id(url):
+        try:
+            get_youtube_video(url)
+        except:
+            try:
+                get_tiktok_video(url)
+            except:
+                try:
+                    get_reels_video(url)
+                except:
+                    pass
+    except:
+        pass
 
-    match = re.search(r'/video/(\d+)', url)
-    if match:
-        return match.group(1)
+#! Main message_handler
+@dp.message()
+async def message_handler(message: types.Message):
+    await message.answer(f"Hello!")
 
-
-def tiktok(url):
-    response = requests.get(url)
-
-    video_id = get_tiktok_video_id(response.url)
-    print(response.url)
-
-    response = requests.get(f'https://tikcdn.io/ssstik/{video_id}')
-
-    if response.status_code == 200:
-
-        print("Success! Video downloaded.")
-        with open(f"{video_id}.mp4", "wb") as file:
-            file.write(response.content)
-    else:
-        print(f"Failed to download video. Status code: {response.status_code}")
-        return None
-
-
+#! Main function
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(API_TOKEN, parse_mode=ParseMode.HTML)
     # And the run events dispatching
     await dp.start_polling(bot)
 
-
-# Пример использования
-# tiktok("https://vm.tiktok.com/ZM6sMUtes/")
-
+#! Start program via main function
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
